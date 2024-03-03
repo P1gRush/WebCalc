97d0e98109998e81dc627150305e97c4129d6aff
P1gRush <142658729+P1gRush@users.noreply.github.com>
2024-03-03 16:57:58 +0500

    add the result list

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const resultElement = document.getElementById('result')
+const listElement = document.getElementById('list')
 const submitBtn = document.getElementById('submit')
 const num1 = document.getElementById('num1')
 const num2 = document.getElementById('num2')
 let sign = ''
+let operation
+const lastResults = []
 const buttonIds = ['plus', 'minus', 'mult', 'div']
-const buttons = buttonIds.map(buttonIds => {
+const buttons = buttonIds.map((buttonIds) => {
 	const button = document.getElementById(buttonIds)
 	return {
 		title: buttonIds,
@@ -18,7 +20,6 @@ buttons.forEach(function (button) {
 	document.getElementById(button.title).onclick = function () {
 		operation = button.title
 		sign = button.sign
-		console.log(sign)
 		sizeChanger(operation)
 	}
 })
@@ -44,6 +45,7 @@ submitBtn.onclick = function () {
 	}
 	if (sign === '') {
 		alert('Пожалуйста, выберите действие')
+		return
 	}
 	const result = calcNumberWithOperations(num1, num2, sign)
 	for (let i = 0; i < buttons.length; i++) {
@@ -51,7 +53,9 @@ submitBtn.onclick = function () {
 		document.getElementById(buttons[i].title).style.backgroundColor =
 			buttons[i].color
 	}
-	printResult(result)
+	lastResults.unshift(result)
+	render()
+	sign = ''
 }
 
 function calcNumberWithOperations(inp1, inp2, sign) {
@@ -72,6 +76,54 @@ function calcNumberWithOperations(inp1, inp2, sign) {
 	}
 }
 
-function printResult(result) {
-	resultElement.textContent = result
+function render() {
+	listElement.innerHTML = ''
+	if (lastResults.length === 0) {
+		listElement.innerHTML = '<p>Нет элементов</p>'
+	}
+	for (let i = 0; i < lastResults.length; i++) {
+		listElement.insertAdjacentHTML(
+			'beforeend',
+			getResultsTemplate(lastResults[i], i),
+		)
+	}
+}
+function getResultsTemplate(result, index) {
+	return `
+	<li class="lastResults__item">
+		<div class="lastResults__title">${result}</div>
+		<div class="buttons" >
+			<div class="buttons__copy-Btn">
+				<img data-index = "${index}" data-type = "copy" id = "copy" src="svg/copy-file-icon.svg" alt="">
+			</div>
+			<div class="buttons__delete-Btn" >
+				<img data-index = "${index}" data-type = "remove" id = "delete" src="svg/close-square-line-icon.svg" alt="">
+			</div>
+		</div>
+	</li>
+	`
+}
+listElement.onclick = function (event) {
+	if (event.target.dataset.index) {
+		const index = Number(event.target.dataset.index)
+		const type = event.target.dataset.type
+		if (type === 'copy') {
+			navigator.clipboard
+				.writeText(lastResults[index])
+				.then(() => {
+					console.log(
+						'Значение скопировано в буфер обмена:',
+						lastResults[index],
+					)
+					alert('Значение скопировано в буфер обмена!')
+				})
+				.catch((err) => {
+					console.error('Ошибка копирования в буфер обмена:', err)
+					alert('Ошибка копирования в буфер обмена!')
+				})
+		} else if (type === 'remove') {
+			lastResults.splice(index, 1)
+			render()
+		}
+	}
 }
